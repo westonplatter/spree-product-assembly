620ca195f0e4f9b0c5cfe17b98f648d32cd8e9a1
Weston Platter <westonplatter@gmail.com>
2013-06-20 00:10:40 -0600

    single to double quotes

--- spec/features/admin/parts_spec.rb
+++ spec/features/admin/parts_spec.rb
@@ -1,27 +1,25 @@
 require 'spec_helper'
 
-describe "Parts", js: true do
+describe 'Parts', js: true do
   stub_authorization!
 
-  let!(:tshirt) { create(:product, name: "T-Shirt") }
-  let!(:mug) { create(:product, name: "Mug") }
+  let!(:tshirt) { FactoryGirl.create(:product, name: 'T-Shirt') }
+  let!(:mug) { FactoryGirl.create(:product, name: 'Mug') }
 
   before do
     visit spree.admin_product_path(mug)
-    visit spree.admin_product_path(mug)
-    check "product_can_be_part"
-    click_on "Update"
+    check 'product_can_be_part'
+    click_on 'Update'
   end
 
-  it "add and remove parts" do
+  it 'add and remove parts' do
     visit spree.admin_product_path(tshirt)
-    visit spree.admin_product_path(tshirt)
-    click_on "Parts"
-    fill_in "searchtext", with: "Mug"
+    click_on 'Parts'
+    fill_in 'searchtext', with: 'Mug'
 
-    within("#search_hits") { click_on "Select" }
+    within('#search_hits') { click_on 'Select' }
     page.should have_content(mug.sku)
 
-    within("#product_parts") { click_on "Remove" }
+    within('#product_parts') { click_on 'Remove' }
   end
 end
