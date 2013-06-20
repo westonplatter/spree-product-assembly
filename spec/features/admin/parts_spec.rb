14f07b8c0a59086fb433468399a45ce00db5e1ef
Weston Platter <westonplatter@gmail.com>
2013-06-20 00:33:17 -0600

    move everything into 1 big test. i know this is wrong.

--- spec/features/admin/parts_spec.rb
+++ spec/features/admin/parts_spec.rb
@@ -3,16 +3,14 @@ require 'spec_helper'
 describe 'Parts', js: true do
   stub_authorization!
 
-  let!(:tshirt) { FactoryGirl.create(:product, name: 'T-Shirt') }
-  let!(:mug) { FactoryGirl.create(:product, name: 'Mug') }
+  it 'update, add, and remove parts' do
+    tshirt = FactoryGirl.create(:product, name: 'T-Shirt')
+    mug = FactoryGirl.create(:product, name: 'Mug')
 
-  before do
     visit spree.admin_product_path(mug)
     check 'product_can_be_part'
     click_on 'Update'
-  end
 
-  it 'add and remove parts' do
     visit spree.admin_product_path(tshirt)
     click_on 'Parts'
     fill_in 'searchtext', with: 'Mug'
